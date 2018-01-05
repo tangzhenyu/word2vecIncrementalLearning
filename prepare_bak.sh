730c55fafd4d63aff51c5a1344a4750b11441025
Tang ZhenYu(搜狐资讯客户端) <zhenyutang@sohu-inc.com>
2018-01-05 16:41:06 +0800

    update

--- prepare_bak.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-hive -e "
-add jar /data0/user/zhangkan/Word2Vec/sh/ForWord2VecDataInput.jar;
-create temporary function getTerm as 'UDF.ForWord2VecDataInput';
-insert overwrite local directory './tempfile'
-select getTerm(XX), getTerm(YY) from XXXX where XXXXXX;
-"
-cat ./tempfile/*>tempfile.txt
-java -jar multiline2oneline.jar -input=./tempfile.txt -output=data 
